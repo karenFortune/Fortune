e976f1da42d4ac8face2a11c298239db9912448c
Karen <karen@fortunesp.com>
2019-04-15 12:22:39 -0700

    Cambios modulo customer service

--- FortuneSystem/Controllers/CustomerServiceController.cs
+++ FortuneSystem/Controllers/CustomerServiceController.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace FortuneSystem.Controllers
 {
-    public class CustomerServiceController : Controller
-    {
-        // GET: Catalogo Customer Service
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CustomerServiceController : Controller
+	{
+		// GET: Catalogo Customer Service
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
 }
